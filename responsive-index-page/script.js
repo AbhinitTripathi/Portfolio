0afc67e8cae3647ec5d6baee4685fb22fa4c13e0
Abhinit Tripathi <114798873+AbhinitTripathi@users.noreply.github.com>
2024-01-14 14:15:14 +0530

    project

--- responsive-index-page/script.js
+++
@@ -1,8 +0,0 @@
-function dropDownMenu() {
-    let navDropDown = document.getElementById("dropdownclick");
-    if (navDropDown.className === "topnav") {
-        navDropDown.className += " responsive";
-    } else {
-        navDropDown.className = "topnav";
-    }
-}
